942bf72dc5d37c54ccdb327d88fd9ec033477a69
Panjie Wicaksono <panjie@panjiesw.com>
2014-04-10 18:20:01 +0700

    Fixing postinstall script

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ actionhero-auth-plugin
 
 Stateless authentication plugin using JSON Web Token for [actionhero][1] API Server. The flow is somewhat similiar to [securesocial][2] authentication plugin for [Play Framework][3].
 
+Documentation - [ah-auth.panjiesw.com](http://ah-auth.panjiesw.com/)
+
 
   [1]: http://actionherojs.com
   [2]: https://github.com/jaliss/securesocial
--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -8,7 +8,7 @@ var localFile   = path.normalize(__dirname + '/../config/auth.js');
 var projectFolder = path.normalize(process.cwd() + '/../config/plugins');
 var projectFile = path.normalize(projectFolder + '/auth.js');
 
-if(!fs.existsSync(projectFile)){
+if(!fs.existsSync(projectFile) && !process.env.TRAVIS){
   console.log("coppying " + localFile + " to " + projectFile);
   mkdirp.sync(projectFolder);
   fs.createReadStream(localFile).pipe(fs.createWriteStream(projectFile));
